91f8a5beb53015311fc5472833fbe91f7541a753
ALANVILASBOAS <84108226+ALANVILASBOAS@users.noreply.github.com>
2021-09-01 18:23:41 -0300

    Update Seman3_1_at3.java
    inclusão de comentário

--- Semana3.1/Seman3_1_at3.java
+++ Semana3.1/Seman3_1_at3.java
@@ -3,7 +3,11 @@ package Familia32;
 import java.util.Scanner;
 
 public class Seman3_1_atv3 {
-
+/*Faça um programa que receba a idade de uma pessoa e mostre na saída em qual
+categoria ela se encontra:
+ 10-14 infantil
+ 15-17 juvenil
+ 18-25 adulto*/	
 	public static void main(String[] args) {
 		
 		Scanner leia = new Scanner(System.in);
