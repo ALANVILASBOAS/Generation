22e61743dad3902cad44b79036a11695645b001a
ALANVILASBOAS <alan07vb@gmail.com>
2021-09-08 23:07:00 -0300

    atv java POO

--- Semana4/.1/AviaoO.java
+++ Semana4/.1/AviaoO.java
@@ -6,7 +6,7 @@ public class AviaoO {
 	objeto e apresente as informações deste objeto no console.*/
 
 	public static void main(String[] args) {
-		Aviao Aviao1= new Aviao("Skyhawk","Gol,n° 0","2020","651325");
+		Aviao Aviao1= new Aviao("Skyhawk","Gol","2020","651325");
 		Aviao Aviao2= new Aviao("Airbus A350 XWB", "Latam","2017","16525");
 		Aviao Aviao3= new Aviao("Airbus A320","TAM","2012","26535");
 		
@@ -14,5 +14,4 @@ public class AviaoO {
 		System.out.println(Aviao2.getDadosCompletos());
 		System.out.println(Aviao3.getDadosCompletos());	
 	}
-
 }
